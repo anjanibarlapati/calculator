90b5a4640093477f419476db8c5bba7e6146a5db
Admin <admin@Admins-MacBook-Air.local>
2024-02-20 15:31:26 +0530

    addition feature

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class addition
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Sum of given two numbers is: "+(a+b));
+    }
+}
