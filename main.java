7247a6269db1ced1c42f24f8fd524b710ac6010d
Admin <admin@Admins-MacBook-Air.local>
2024-02-20 15:33:10 +0530

    subtraction feature

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class subtraction
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Difference of given two numbers is: "+(a-b));
+    }
+}
